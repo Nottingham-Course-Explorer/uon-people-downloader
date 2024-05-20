b130e9b3ec28e1db957c5bd304f7b8dadb469753
Nathaniel Taulbut <ntaulbut@gmail.com>
2024-05-20 03:59:24 +0100

    add schema

---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE staff
+(
+    username   text PRIMARY KEY NOT NULL,
+    salutation text NOT NULL,
+    forename   text NOT NULL,
+    surname    text NOT NULL,
+    department text NOT NULL,
+    title      text NOT NULL,
+    email      text NOT NULL,
+    crawl_time int  NOT NULL
+);
